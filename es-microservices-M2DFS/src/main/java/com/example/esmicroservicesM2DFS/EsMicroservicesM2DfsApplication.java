4f7c8d5c2e4936245780fc21425e69894b4a3e17
ALANFRANCHI <alexandre.lanfranchi@webmail.universita.corsica>
2020-10-24 11:39:08 +0200

    INIT EUREKA SERVER

---
+++ es-microservices-M2DFS/src/main/java/com/example/esmicroservicesM2DFS/EsMicroservicesM2DfsApplication.java
@@ -0,0 +1,13 @@
+package com.example.esmicroservicesM2DFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsMicroservicesM2DfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsMicroservicesM2DfsApplication.class, args);
+	}
+
+}
---
+++ es-microservices-M2DFS/src/test/java/com/example/esmicroservicesM2DFS/EsMicroservicesM2DfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esmicroservicesM2DFS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsMicroservicesM2DfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
